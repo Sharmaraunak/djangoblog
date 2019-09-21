ae3231ff3876d380ec5783abd6e694410b977f71
Raunak <sharmaraunak338@gmail.com>
2019-09-21 16:34:03 +0530

    templates
    Did templating of basic html pages

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author':'Raunak Sharma',
+        'title':'First Post',
+        'content':'Look I have cretaed a post.soorty two errors.',
+        'date_posted':'August 27,2018'
+    },
+     {
+        'author':'Raunak Sharma',
+        'title':'second Post',
+        'content':'Look I have cretaed another post.soorty two errors.',
+        'date_posted':'August 27,2018'
+    },
+]
+
 def home(request):
-    return HttpResponse('<h1>BLOG HOME</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>BLOG ABOUT</h1>')
+    return render(request,'blog/about.html',{'title':'about'})
 
 
