200bbdc0d2e77b43a5c6df3c3e657316077087ed
Raunak <sharmaraunak338@gmail.com>
2019-10-18 11:30:45 +0530

    post-details
    post details

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import postListView,postDetailView
 
 urlpatterns = [
-    path('', views.home,name='blog-home'),
+    path('', postListView.as_view(),name='blog-home'),
+    path('post/<int:pk>/',postDetailView.as_view(),name='post-detail'),
      path('about/', views.about,name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,28 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView,DetailView
 from blog.models import Post
 
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request,'blog/home.html',context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request,'blog/home.html',context)
+
+
+class postListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class postDetailView(DetailView):
+    model = Post
+   
 
 def about(request):
     return render(request,'blog/about.html',{'title':'about'})
